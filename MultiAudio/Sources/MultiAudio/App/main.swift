e500b5774a34177ac4701dd2f78a5dc03a3105d0
Bhavya-Goswami <bhavyagoswami20108@gmail.com>
2026-07-16 11:50:13 +0200

    Delete MultiAudio directory

--- MultiAudio/Sources/MultiAudio/App/main.swift
+++
@@ -1,7 +0,0 @@
-import AppKit
-
-let app = NSApplication.shared
-let delegate = AppDelegate()
-app.delegate = delegate
-app.setActivationPolicy(.accessory) // Menu bar app (no Dock icon by default)
-app.run()
